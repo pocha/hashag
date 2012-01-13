afa14c9fb5cbd96e5c516fa1590c30362a6a85a7
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-01-13 14:55:54 +0530

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterAngellistBridge::Application.config.secret_token = 'b7183a6d25961e16a98943e7cd0827d4607ea9b2a7a7b51987c858b1b7e0f66aeb2110a6102556f8b1b0c69b86e4b6e35634e9a2143bfa2addaa75cffdcfb4c0'
