17705386101eb60ea1156be5380395c09a9eb697
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-01-13 17:37:36 +0530

    Add active:boolean to users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120113101053) do
+ActiveRecord::Schema.define(:version => 20120113120723) do
 
   create_table "users", :force => true do |t|
     t.string   "provider"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20120113101053) do
     t.string   "angellist_oauth_token_secret"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "active"
   end
 
 end
