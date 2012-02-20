de6f2c3353f29a46b42edfa97df7e4eb8e0c2b85
pocha <pocha.sharma@gmail.com>
2012-02-20 10:51:07 +0000

    storing the nickname & not actual twitter name. Needed for the twitter search

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["screen_name"]
+      user.name = auth["info"]["nickname"]
       user.twitter_oauth_token = auth["credentials"]["token"]
       user.twitter_oauth_token_secret = auth["credentials"]["secret"]
     end
