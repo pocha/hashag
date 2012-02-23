1101b22e69af9177beb92d6b53eecba699581af1
pocha <pocha@stalkninja.com>
2012-02-23 18:47:20 +0530

    changed last_tweet_id to string as integer was not sufficient

--- db/migrate/20120221100030_add_last_tweet_id_domain_to_users.rb
+++ db/migrate/20120221100030_add_last_tweet_id_domain_to_users.rb
@@ -1,5 +1,5 @@
 class AddLastTweetIdDomainToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :last_tweet_id, :integer, :null => false, :default => 0
+    add_column :users, :last_tweet_id, :string, :null => false, :default => 0
   end
 end
